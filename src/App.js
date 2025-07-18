8cc08a986f316727f709bdd0c0f65e2b8cd64578
HabtamuAlemayehu <halex0975185140@gmail.com>
2025-07-18 15:20:10 +0300

    q

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Header from './Conponent/Header/Header'
+import Menu from './Conponent/Menu/Menu'
 function App() {
+  const mycss = {
+    color: "white"
+  }
+  const second = {
+        color: "#33C3AC"
+
+  }
   return (
     <div className="App">
-      <h1>react</h1>
+      <Header/>
+      <main className="main-content">
+        <section>
+          <h1 style={mycss}>Welcome <span style={second}>to My Portfolio</span></h1>
+          <p>This is a long scrollable section.</p>
+          {/* Add a lot of content here to test scroll */}
+        </section>
+      </main>
     </div>
   );
 }
